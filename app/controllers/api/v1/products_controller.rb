df7c8d8fd7566aea1ec8f639bd6f3fce5bed69e4
--get <danilomelorocha@gmail.com>
2018-12-16 01:05:20 +0100

    Add function to create and update product. And set required fields.

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -16,6 +16,29 @@ module Api
         product.destroy
         render json: {status:'SUCCESS', message:'Product deleted', data:product}, status: :ok
       end
+
+      def update
+        product = Product.find(params[:id]);
+        if product.update_attributes(product_params)
+          render json: {status:'SUCCESS', message:'Product updated', data:product}, status: :ok
+        else
+          render json: {status:'ERROR', message:'Product not updated', data:product.errors}, status: :unprocessable_entity
+        end
+      end
+
+      def create
+        product = Product.new(product_params)
+        if product.save
+          render json: {status:'SUCCESS', message:'Product saved', data:product}, status: :ok
+        else
+          render json: {status:'ERROR', message:'Product not saved', data:product.errors}, status: :unprocessable_entity
+        end
+      end
+
+      private
+      def product_params
+        params.permit(:number, :name, :price, :description)
+      end
     end
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  validates :number, presence: true
+  validates :name, presence: true
 end
